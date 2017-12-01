84c3e921ed1b03bb37248c174a1f904108136dc4
Capri84 <capri84@mail.ru>
2017-12-02 00:36:43 +0300

    Added localization and new functionality.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,10 +7,17 @@
  */
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 /**
@@ -31,62 +38,87 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice (quantity * price);
+        CheckBox whippedCheckBox = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean creamState = whippedCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate);
+        boolean chocoState = chocolateCheckBox.isChecked();
+        EditText nameField = (EditText) findViewById(R.id.name);
+        String name = nameField.getText().toString();
+        int price = calculatePrice(creamState, chocoState);
+        String priceMessage = createOrderSummary(price, name, creamState, chocoState);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.forWhom, name));
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
-     * This method is called when the Espresso button is clicked.
+     * Calculates the price of the order.
+     * @return total price.
      */
-    public void espresso(View view) {
-        price = 3;
-        display (quantity);
+    private int calculatePrice(boolean creamState, boolean chocoState) {
+        int basePrice = 5;
+        if (creamState == true) {
+            basePrice = basePrice + 1;
+        }
+        if (chocoState == true) {
+            basePrice = basePrice + 2;
+        }
+       return price = basePrice * quantity;
     }
 
     /**
-     * This method is called when the Americano button is clicked.
+     * Creates order summary.
+     * @param price is the price calculated in calculatePrice().
+     * @param name is the name of the client.
+     * @param creamState shows whether the whippedCheckBox is checked or not.
+     * @param chocoState shows whether the chocolateCheckBox is checked or not.
      */
-    public void americano(View view) {
-        price = 5;
-        display (quantity);
-    }
-
-    /**
-     * This method is called when the Cappuccino button is clicked.
-     */
-    public void cappuccino(View view) {
-        price = 7;
-        display (quantity);
+    private String createOrderSummary (int price, String name, boolean creamState, boolean chocoState) {
+        String priceMessage = getString(R.string.nameSummary, name);
+        priceMessage += "\n" + getString(R.string.addWhippedCream, creamState);
+        priceMessage += "\n" + getString(R.string.addChocolate, chocoState);
+        priceMessage += "\n" + getString(R.string.quantity2, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_price,NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += "\n" + getString(R.string.thanks);
+        return priceMessage;
     }
 
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast toast = Toast.makeText(this, getString(R.string.toastMoreCoffee), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
         quantity = quantity + 1;
-        display (quantity);
+        displayQuantity (quantity);
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast toast = Toast.makeText(this, getString(R.string.toastLessCoffee), Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
         quantity = quantity - 1;
-        display (quantity);
+        displayQuantity (quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int num) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        quantityTextView.setText("" + num);
     }
 }
